d57ad910b1d2e936adce2d59eb3be46496b7c5c5
Guido Barosio <gbarosio@gmail.com>
2016-05-13 11:27:53 -0500

    :)

--- db/database.sh
+++ db/database.sh
@@ -8,6 +8,7 @@ sleep 2
 echo "Starting server now"
 pg_ctl -l logfile start 
 sleep 2
-dropuser ha-rw 
-dropdb happyanimals 
+#dropuser ha-rw 
+#dropdb happyanimals 
 createuser -r -d -s ha-rw && createdb -O ha-rw happyanimals &&  cat init_database.sql | psql -U ha-rw happyanimals
+cat animals.sql | psql -U ha-rw happyanimals
