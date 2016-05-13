6a2838433a34c27825b70c198d8d5609f4865085
Guido Barosio <gbarosio@gmail.com>
2016-05-13 07:23:10 -0500

    empitucado

--- Bot.pl
+++ Bot.pl
@@ -12,28 +12,23 @@ use Scalar::Util 'blessed';
 use Try::Tiny;
 use Data::Dumper;
 use DBI;
-use vars qw/$nt/;
+use vars qw/$nt $consumer_key $consumer_secret $token $token_secret/;
 
 use Log::Log4perl qw(:easy);
 Log::Log4perl->easy_init($ERROR);
 
 my $quote = $ARGV[0];
 
-my $config = LoadFile('conf.yml');
-
-print Dumper $config;
-
-#my $consumer_key = '1IkveZAc2FLaCdD7FQv6hZ6gZ';
-#my $consumer_secret ='g6Dz3jZbQjXAPQ9UZbJTlmfgxgSaC4dNw4d6enjhNm5jbsAyBk';
-#my $token ='729413218267959296-X7kHPl6RxQQzampE33IB0QWKQgfyeeS';
-#my $token_secret='XcZ0TlErKkC6fFOibH84owU7i3mtrytgjsRDKoS79oUO3';
-
-my $consumer_key  	= $config->{consumer_key}; 
-my $consumer_secret 	= $config->{consumer_secret};
-my $token 		= $config->{access_token};
-my $token_secret	= $config->{access_token_secret};
-
+&parseConf();
 &connect();
+
+sub parseConf {
+	my $config = LoadFile('conf.yml');
+	$consumer_key  	= $config->{consumer_key}; 	
+	$consumer_secret 	= $config->{consumer_secret};
+	$token 		= $config->{access_token};
+	$token_secret	= $config->{access_token_secret};
+}
 
 sub connect {
 
@@ -65,14 +60,14 @@ sub main {
 		$result = $nt->update($quote);
 	} else  {
 		$text = getText();
-		#$result = $nt->update($text);
+		$result = $nt->update($text);
 		print "Not tweeted: $text\n";
 	}
 }
 
 # Extracts a quote from a file, where a line represents each quote
 sub getText {
-	my $file = 'quotes3.txt';
+	my $file = 'wikipages.out';
 	open FILE, "<$file" or die "Could not open $file: $!\n";
 	my @array=<FILE>;
 	close FILE;
